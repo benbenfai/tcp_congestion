aec1fb11aab0fdf30bc8d1aad71b33a64d700ac3
benbenben <49261856+benbenfai@users.noreply.github.com>
2025-10-25 22:27:59 +0800

    Update tcp_elegant.c

--- tcp_elegant.c
+++ tcp_elegant.c
@@ -296,6 +296,7 @@ static void tcp_elegant_set_state(struct sock *sk, u8 new_state)
 
 	if (new_state == TCP_CA_Loss) {
 		ca->rtt_max = ca->rtt_curr;
+		ca->base_rtt == 0x7fffffff;
 		rtt_reset(tp, ca);
 		ca->cache_wwf = 0;
 		ca->round_start = 1;

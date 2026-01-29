7c7f56903da1ebedb4e5c1c1beea778b3852888d
benbenben <49261856+benbenfai@users.noreply.github.com>
2026-01-29 20:12:55 +0800

    Update tcp_elegant.c

--- tcp_elegant.c
+++ tcp_elegant.c
@@ -274,8 +274,8 @@ static void elegant_cong_avoid(struct sock *sk, struct elegant *ca, const struct
 		return;
 
 	if (tcp_in_slow_start(tp)) {
-		tcp_slow_start(tp, rs->acked_sacked);
 		tp->snd_ssthresh = copa_ssthresh(ca);
+		tcp_slow_start(tp, rs->acked_sacked);
 	} else {
 		u32 wwf;
 		u64 wwf64 = tp->snd_cwnd * ca->rtt_max << ELEGANT_UNIT_SQ_SHIFT;
